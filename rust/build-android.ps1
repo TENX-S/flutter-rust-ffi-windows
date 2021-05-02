8151a9ba0cb0915e5c968689a2a68f2c5b65cf67
Ares <ttenx@pm.me>
2021-05-02 10:31:04 +0800

    Add error handling

--- rust/build-android.ps1
+++ rust/build-android.ps1
@@ -1,18 +1,15 @@
 Set-StrictMode -Off;
 
-if (-NOT (Test-Path $env:ANDROID_NDK_HOME)) {
-    Write-Host 'Error: Please, set the ANDROID_NDK_HOME env variable to point to your NDK folder'
-    exit 1
-} else {
-    Write-Host "Found ANDROID_NDK_HOME=$env:ANDROID_NDK_HOME"
+function Write-Done {
+    Write-Host 'DONE' -ForegroundColor Green
+    Write-Host ''
 }
 
-$targets = @(
-    'aarch64-linux-android',
-    'armv7-linux-androideabi',
-    'i686-linux-android',
-    'x86_64-linux-android'
-)
+function Write-Error {
+    Write-Host 'Fatal Error:' -ForegroundColor Red
+    Write-Host $_
+    exit 1
+}
 
 function Add-Target {
 
@@ -24,27 +21,79 @@ function Add-Target {
         [string]$sdkVersion
     )
 
-    $file = ''
-    if (Test-Path './.cargo/config') {
-        $file = './.cargo/config'
-    } elseif (Test-Path './.cargo/config.toml') {
-        $file = './.cargo/config.toml'
-    }
+    try {
+        $file = ''
+        if (Test-Path './.cargo/config') {
+            $file = './.cargo/config'
+        } elseif (Test-Path './.cargo/config.toml') {
+            $file = './.cargo/config.toml'
+        }
 
-    Add-Content $file "[target.$targetName]"
-    $ar_location = "$env:ANDROID_NDK_HOME\toolchains\llvm\prebuilt\windows-x86_64\$targetName\bin\ar.exe".Replace('\', '\\')
-    $ar_field = 'ar = "' + $ar_location + '"'
-    Add-Content $file $ar_field
-    $linker_location = "$env:ANDROID_NDK_HOME\toolchains\llvm\prebuilt\windows-x86_64\bin\$targetName$sdkVersion-clang.cmd".Replace('\', '\\')
-    if ($targetName -eq 'armv7-linux-androideabi') {
-        $linker_location = "$env:ANDROID_NDK_HOME\toolchains\llvm\prebuilt\windows-x86_64\bin\armv7a-linux-androideabi$sdkVersion-clang.cmd".Replace('\', '\\')
+        Add-Content $file "[target.$targetName]"
+        $ar_location = "$env:ANDROID_NDK_HOME\toolchains\llvm\prebuilt\windows-x86_64\$targetName\bin\ar.exe".Replace('\', '\\')
+        $ar_field = 'ar = "' + $ar_location + '"'
+        Add-Content $file $ar_field
+        $linker_location = "$env:ANDROID_NDK_HOME\toolchains\llvm\prebuilt\windows-x86_64\bin\$targetName$sdkVersion-clang.cmd".Replace('\', '\\')
+        if ($targetName -eq 'armv7-linux-androideabi') {
+            $linker_location = "$env:ANDROID_NDK_HOME\toolchains\llvm\prebuilt\windows-x86_64\bin\armv7a-linux-androideabi$sdkVersion-clang.cmd".Replace('\', '\\')
+        }
+        $linker_field = 'linker = "' + $linker_location + '"'
+        Add-Content $file $linker_field
+        if ($targetName -ne 'x86_64-linux-android') {
+            Add-Content $file ""
+        }
     }
-    $linker_field = 'linker = "' + $linker_location + '"'
-    Add-Content $file $linker_field
-    if ($targetName -ne 'x86_64-linux-android') {
-        Add-Content $file ""
+    catch {
+        Write-Error
     }
 }
+
+function Build {
+
+    param(
+        [Parameter()]
+        [string]$targetName,
+
+        [Parameter()]
+        [string]$folderName
+    )
+
+    try {
+        Write-Host "Building"; Write-Host "    $targetName" -ForegroundColor Yellow
+        cargo build --target $targetName --release
+        $libPath = Get-ChildItem -Path "./target/$targetName/release" lib*.so | Sort-Object LastWriteTime | Select-Object -First 1
+        $libName = Split-Path $libPath -leaf
+        $releaseFolder = "$jinLibs/$folderName"
+        if (-NOT (Test-Path -Path $releaseFolder)) {
+            New-Item -Path $jinLibs -Name $folderName -ItemType "directory"
+        }
+        Copy-Item "./target/$targetName/release/$libName" -Destination $releaseFolder -Force
+        Write-Done
+    }
+    catch {
+        Write-Error
+    }
+}
+
+if (-NOT (Test-Path $env:ANDROID_NDK_HOME)) {
+    Write-Host 'Error: Please, set the ANDROID_NDK_HOME env variable to point to your NDK folder'
+    exit 1
+} else {
+    Write-Host "Found ANDROID_NDK_HOME=$env:ANDROID_NDK_HOME"
+}
+
+$jinLibs = "../android/app/src/main/jniLibs"
+
+if (-NOT (Test-Path -Path $jinLibs)) {
+    New-Item -Path "../android/app/src/main" -Name "jniLibs" -ItemType "directory"
+}
+
+$targets = @(
+    'aarch64-linux-android',
+    'armv7-linux-androideabi',
+    'i686-linux-android',
+    'x86_64-linux-android'
+)
 
 if (-NOT (Test-Path './.cargo')) {
     New-Item -Path . -Name '.cargo' -ItemType "directory"
@@ -55,38 +104,14 @@ if (-NOT (Test-Path './.cargo')) {
     foreach ($t in $targets) { Add-Target -targetName $t -sdkVersion '29' }
 }
 
-$jinLibs = "../android/app/src/main/jniLibs"
-
-function Write-Done {
-    Write-Host 'DONE' -ForegroundColor Green
-    Write-Host ''
-}
-
-function Build-Android {
-
-    param(
-        [Parameter()]
-        [string]$targetName,
-
-        [Parameter()]
-        [string]$folderName
-    )
-
-    Write-Host "Building"; Write-Host "    $targetName" -ForegroundColor Yellow
-    cargo build --target $targetName --release
-    $libPath = Get-ChildItem -Path "./target/$targetName/release" lib*.so | Sort-Object LastWriteTime | Select-Object -First 1
-    $libName = Split-Path $libPath -leaf
-    $releaseFolder = "$jinLibs/$folderName"
-    if (-NOT (Test-Path -Path $releaseFolder)) {
-        New-Item -Path $jinLibs -Name $folderName -ItemType "directory"
-    }
-    Copy-Item "./target/$targetName/release/$libName" -Destination $releaseFolder -Force
-    Write-Done
-}
-
 Write-Host 'Making bindings:'
 Write-Host '    rust/target/bindings.h' -ForegroundColor Yellow
-cbindgen ./src/lib.rs --output target/bindings.h --lang c
+try {
+    cbindgen ./src/lib.rs --output target/bindings.h --lang c
+}
+catch {
+    Write-Error
+}
 Write-Done
 
 $info = @(
@@ -96,14 +121,16 @@ $info = @(
     [System.Tuple]::Create('x86_64-linux-android', 'x86_x64')
 )
 
-if (-NOT (Test-Path -Path $jinLibs)) {
-    New-Item -Path "../android/app/src/main" -Name "jniLibs" -ItemType "directory"
-}
-
 foreach ($i in $info) {
-    Build-Android -targetName $i.Item1 -folderName $i.Item2
+    Build -targetName $i.Item1 -folderName $i.Item2
 }
 
 Write-Host 'Generating Bindings for Dart'
-flutter pub run ffigen
+try {
+    flutter pub run ffigen
+}
+catch {
+    Write-Error
+}
+
 Write-Done
